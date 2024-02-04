5fda8b874e27908333a584c946fb8a9c5d6c9061
Kgaugelo Ramaramela <kgaugelo.ramaramela@standardbank.co.za>
2024-02-04 17:11:44 +0200

    code cov test

---
+++ musica-phone/src/main/java/com/musica/phone/TheClasOfLegends.kt
@@ -0,0 +1,12 @@
+package com.musica.phone;
+
+class TheClasOfLegends {
+
+    fun add(a: Int, b: Int): Int {
+        return a + b
+    }
+
+    fun sub(a: Int, b: Int): Int {
+        return a - b
+    }
+}
---
+++ musica-phone/src/main/java/com/musica/phone/TheThingOfLegends.java
@@ -0,0 +1,12 @@
+package com.musica.phone;
+
+public class TheThingOfLegends {
+
+    int multi(int a, int b) {
+        return a * b;
+    }
+
+    int divide(int a, int b) {
+        return a / b;
+    }
+}
